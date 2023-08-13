a5c5fc207619e02153497eec6b331613ffacd4ea
Rhode-park <jinah.park.16@gmail.com>
2023-08-13 19:39:13 +0900

    feat: SceneDelegate 구현

---
+++ Medi-Buddy/Medi-Buddy/MediCalendarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MediCalendarViewController.swift
+//  Medi-Buddy
+//
+//  Created by Jinah Park on 2023/08/13.
+//
+
+import UIKit
+
+final class MediCalendarViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = .systemBackground
+    }
+}
---
+++ Medi-Buddy/Medi-Buddy/MediListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MediListViewController.swift
+//  Medi-Buddy
+//
+//  Created by Jinah Park on 2023/08/13.
+//
+
+import UIKit
+
+final class MediListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = .systemBackground
+    }
+}
