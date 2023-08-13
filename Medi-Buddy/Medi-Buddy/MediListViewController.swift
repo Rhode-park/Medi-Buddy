9c3fecfb27c2111ed2662710097eb3785c2728d7
Rhode-park <jinah.park.16@gmail.com>
2023-08-13 20:20:26 +0900

    feat: MediListViewController 구현

--- Medi-Buddy/Medi-Buddy/MediListViewController.swift
+++ Medi-Buddy/Medi-Buddy/MediListViewController.swift
@@ -8,13 +8,119 @@
 import UIKit
 
 final class MediListViewController: UIViewController {
+    lazy var mediListCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: configureListLayout())
+        collectionView.register(MediListCell.self, forCellWithReuseIdentifier: MediListCell.reuseIdentifier)
+        collectionView.register(HeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: HeaderView.reuseIdentifier)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return collectionView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        configureNavigationBar()
+        configureSubView()
+        configureConstraint()
+    }
+    
+    func configureListLayout() -> UICollectionViewCompositionalLayout {
+        var configuration = UICollectionLayoutListConfiguration(appearance: .plain)
+        configuration.trailingSwipeActionsConfigurationProvider = { indexPath in
+            let deleteAction = UIContextualAction(style: .destructive, title: "삭제") { action, view, actionPerformed in
+                print("deleteMedicine")
+                self.mediListCollectionView.reloadData()
+                actionPerformed(true)
+            }
+
+            return UISwipeActionsConfiguration(actions: [deleteAction])
+        }
+        configuration.headerMode = .supplementary
+        
+        let layout = UICollectionViewCompositionalLayout.list(using: configuration)
+        
+        return layout
     }
     
     private func configureUI() {
         view.backgroundColor = .systemBackground
+        mediListCollectionView.dataSource = self
+    }
+    
+    private func configureSubView() {
+        view.addSubview(mediListCollectionView)
+    }
+    
+    private func configureConstraint() {
+        NSLayoutConstraint.activate([
+            mediListCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            mediListCollectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            mediListCollectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            mediListCollectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+
+    
+    private func configureNavigationBar() {
+        let addMedicineButton = UIBarButtonItem(barButtonSystemItem: .add,
+                                                target: self,
+                                                action: #selector(addMedicine))
+        addMedicineButton.tintColor = .systemCyan
+        
+        let settingButton = UIBarButtonItem(image: UIImage(systemName: "ellipsis.circle"),
+                                            style: .plain,
+                                            target: self,
+                                            action: #selector(setMedicineList))
+        settingButton.tintColor = .systemCyan
+        
+        navigationItem.leftBarButtonItem = addMedicineButton
+        navigationItem.rightBarButtonItem = settingButton
+        navigationItem.title = Date().convertDate()
+    }
+    
+    @objc
+    private func addMedicine() {
+        print("addMedicine")
+    }
+    
+    @objc
+    private func setMedicineList() {
+        print("setMedicineList")
+    }
+}
+
+extension MediListViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return MedicineManager.shared.categoryList.count
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let medicine = MedicineManager.shared.list.filter { $0.category == MedicineManager.shared.categoryList[indexPath.section] }[indexPath.item]
+        
+        guard let cell = mediListCollectionView.dequeueReusableCell(withReuseIdentifier: "MediListCell",
+                                                                    for: indexPath) as? MediListCell else { return MediListCell() }
+        cell.configureCell(medicine: medicine)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        guard kind == UICollectionView.elementKindSectionHeader,
+              let header = collectionView.dequeueReusableSupplementaryView(
+                ofKind: kind,
+                withReuseIdentifier: "HeaderView",
+                for: indexPath
+              ) as? HeaderView else { return UICollectionReusableView() }
+        
+        let category = MedicineManager.shared.categoryList[indexPath.section]
+        
+        header.configureHeader(category: category)
+        
+        return header
     }
 }
