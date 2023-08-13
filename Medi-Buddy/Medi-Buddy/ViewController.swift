fddc5f17110015d1bf8c698d1350def3d8e24308
Rhode-park <jinah.park.16@gmail.com>
2023-08-13 19:26:42 +0900

    chore: initial commit

---
+++ Medi-Buddy/Medi-Buddy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Medi-Buddy
+//
+//  Created by Jinah Park on 2023/08/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
